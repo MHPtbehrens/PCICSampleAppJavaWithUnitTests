bb3406560e9a30619f61c906f0fd3c23374db742
Tristan Behrens <tristan.behrens@mhp.com>
2016-09-29 14:43:21 +0200

    Initial commit.

---
+++ src/main/java/com/porsche/PcicSampleAppWithUnitTestsApplication.java
@@ -0,0 +1,12 @@
+package com.porsche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcicSampleAppWithUnitTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcicSampleAppWithUnitTestsApplication.class, args);
+	}
+}
